57c922116d906ddfb6eb9033b1212acbba44d7ce
João Manoel Neto <joaaomanooel@gmail.com>
2020-08-06 01:47:02 -0300

    create insert class route

---
+++ src/database/index.ts
@@ -0,0 +1,3 @@
+import db from './connection';
+
+export default db;
--- src/routes/classes.ts
+++ src/routes/classes.ts
@@ -1,6 +1,22 @@
 import { Router } from 'express';
+import db from '../database';
 
 const baseUrl = '/classes';
 
 export default Router()
-  .post(baseUrl, (req, res) => res.send(req.body));
+  .post(baseUrl, async (req, res) => {
+    const {
+      name,
+      avatar,
+      whatsapp,
+      bio,
+      subject,
+      price,
+      // schedule,
+    } = req.body;
+
+    const [user_id] = await db('users').insert({ name, avatar, whatsapp, bio });
+    await db('classes').insert({ subject, price, user_id });
+
+    return res.send({ user_id });
+  });
