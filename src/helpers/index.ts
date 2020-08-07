9cf021561369c4044b40ca69bc0c18a7b92b3162
João Manoel Neto <joaaomanooel@gmail.com>
2020-08-07 15:32:07 -0300

    add helpers

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,4 +1,10 @@
+/**
+ * Convert hour string to minutes
+ * @param time string > HH:mm
+ * @returns Return the converted time to minutes as number
+ * @example convertHourToMinites('08:30'); // 510
+ */
 export const convertHourToMinites = (time: string) => {
-  const [hour, minutes] = time.split(':').map(Number);
+  const [hour = 0, minutes = 0] = time.split(':').map(Number);
   return (hour * 60) + minutes;
 };
